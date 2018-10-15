e051ca40715c408f087b0f18f3890b96a50e950d
putul sahani <putul.sahani@neosofttech.com>
2018-10-15 10:45:27 +0530

    change fotter text

--- src/components/footer/footerComponent.js
+++ src/components/footer/footerComponent.js
@@ -10,7 +10,7 @@ class FooterComponent extends React.Component {
               <div className='m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop'>
                 <div className='m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last'>
                   <span className='m-footer__copyright'>
-                  2018 &copy; TelkomECO Core
+                  2018 &copy; ECO Conductor
                   </span>
                 </div>
               </div>
